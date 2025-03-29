fef40d065600c03b3e10e6cd900694eb889ff19d
Tomas Martin <martintomas898@gmail.com>
2025-03-29 15:40:41 -0300

    added cron

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,32 @@
+import axios from "axios"
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enabled: true,
+    tasks: {
+      callApiEveryTenMinutes: {
+        task: async ({ strapi }: { strapi: any }) => {
+          try {
+            console.log(
+              "Ejecutando cron job - Hora actual:",
+              new Date().toISOString()
+            );
+          } catch (error) {
+            console.error(
+              "Error al llamar a la API:",
+              (error as Error).message
+            );
+          }
+        },
+        options: {
+          rule: "*/10 * * * *",
+          tz: "America/Mexico_City",
+        },
+      },
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
