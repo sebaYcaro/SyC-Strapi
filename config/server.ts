ea201a711ff2ea341dd5efe4746a1ec7c72e2df4
Tomas Martin <martintomas898@gmail.com>
2025-03-29 16:24:03 -0300

    actualizo el cron

--- config/server.ts
+++ config/server.ts
@@ -13,6 +13,9 @@ export default ({ env }) => ({
               "Ejecutando cron job - Hora actual:",
               new Date().toISOString()
             );
+            // Hacer la llamada a la API
+            const response = await axios.get(env('URL'));
+            console.log("Respuesta de la API:", response.data);
           } catch (error) {
             console.error(
               "Error al llamar a la API:",
